b47b5ce166b32dd942d83846daef693fdfdd98f2
Christopher El-Murr <christopher.el-murr@stu.ocsb.ca>
2025-09-19 18:07:42 +0000

    added areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+
+//Created By : Christopher El - Murr
+// copyright 2025 Christopher El-murr
+//Date : 09 19 2025
+//is program is a basic math calculator capable of calculating the area and perimeter of a rectangle
+
+
+#include <cmath>
+#include <iostream>
+int main() {
+    std::cout << "If a rectangle has the dimensions:" << std ::endl;
+    std::cout << "8cm x 2m" << std ::endl;
+    std::cout << "" << (8* 2) << std ::endl;
+    std::cout << "Area is = " << (6 * 9)<< "cm^2" << std ::endl;
+}
